1ea4c9686fe1a1968bf5ba99f13b0090d708353a
Dmitry Ageev <dimichageev@icloud.com>
2020-06-29 17:09:59 +0300

    Остановился на 12:00

--- js/script.js
+++ js/script.js
@@ -39,4 +39,24 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    // Timer
+
+    let deadLine = '2020-10-21';
+
+    function getTimeRemaining(endtim) {
+        let t = Date.parse(endtim) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*24)));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
 });
