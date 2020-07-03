84eabe5d2f4803f633fbd1dee323d0b66054e86b
Dmitry Ageev <dimichageev@icloud.com>
2020-07-03 16:18:16 +0300

    Выполнил задание из видео

--- js/script.js
+++ js/script.js
@@ -41,15 +41,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    let deadLine = '2020-10-21';
+    let deadline = '2020-10-21';
 
-    function getTimeRemaining(endtim) {
-        let t = Date.parse(endtim) - Date.parse(new Date()),
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
             hours = Math.floor((t/(1000*60*60)));
-            // hours = Math.floor((t/1000/60/60) % 24),
-            // days = Math.floor((t/(1000*60*60*24)));
+            // hours = Math.floor((t/1000/60/60) % 24);
+            // days = Math.floor( (t/(1000*60*60*24)) );
 
             return {
                 'total' : t,
@@ -59,4 +59,34 @@ window.addEventListener('DOMContentLoaded', function() {
             };
     }
 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return'0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
